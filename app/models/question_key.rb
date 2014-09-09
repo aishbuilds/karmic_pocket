a6a983fb7d3684225f41059eff2e7203baff4aac
Aishwarya <aishwarya923@gmail.com>
2014-09-09 22:49:01 +0530

    Established connection in QuestionKey model.

--- app/models/question_key.rb
+++ app/models/question_key.rb
@@ -2,5 +2,7 @@ require 'rubygems'
 require 'active_record'
 
 class QuestionKey < ActiveRecord::Base
+	QuestionKey.establish_connection(YAML.load_file("config/database.yml"))
+	
 	belongs_to :question
 end
